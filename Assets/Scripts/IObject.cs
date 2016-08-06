9c90212775c55d3ba0bd595e3b4e3b5b1483ceca
Sebastien PARIAUD <spariaud@e2r4p10.42.fr>
2016-08-06 15:48:28 +0200

    Add interface IObject

---
+++ Assets/Scripts/IObject.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface IObject {
+
+	public void Enable (); 
+	public void Disbale ();
+	public bool GetState ();
+	// public float GetWeight ();
+	// public float GetWidth ();
+	// public float GetHeight ();
+}
