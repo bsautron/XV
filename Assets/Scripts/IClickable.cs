bdef6ed35eb6cf4a73d9a06353265fb59074fd5d
Basile Durst <bdurst2812@basiles-MacBook-Pro.local>
2016-08-18 00:10:10 +0200

    Clickable: IClickable et Class Clickable

---
+++ Assets/Scripts/IClickable.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface IClickable {
+
+	// what to do when you right click
+	void rightClick();
+
+	// what to do when you left click
+	void leftClick();
+}
---
+++ Assets/Scripts/test.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour, IClickable {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void rightClick() {
+		Debug.Log("right click!");
+	}
+
+	public void leftClick() {
+		Debug.Log("left click!");
+	}
+}
