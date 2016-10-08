c836f8e61849f9aac07bf31b026cd21ad148aabe
Sebastien PARIAUD <spariaud@e3r4p14.42.fr>
2016-10-09 01:22:05 +0200

    managecamera: Add CameraManager and KeymapManager

---
+++ Assets/Scripts/Camera/CameraStatic.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraStatic : MonoBehaviour {
+
+	private float	_rotationY = 0;
+	private bool	_direction = true;
+
+	void Start () {
+		this._rotationY = this.transform.transform.eulerAngles.y;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (this._direction && transform.eulerAngles.y < this._rotationY + 30f) {
+			transform.Rotate (new Vector3 (0, 0.1f, 0));
+		} else {
+			this._direction = false;
+		}
+		if (!this._direction && transform.eulerAngles.y > this._rotationY - 30f) {
+			transform.Rotate (new Vector3 (0, -0.1f, 0));
+		} else {
+			this._direction = true;
+		}
+	}
+}
---
+++ Assets/Scripts/Managers/KeymapManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeymapManager : Singleton<KeymapManager> {
+
+	public KeyCode	nextCamera = KeyCode.Plus;
+	public KeyCode	prevCamera = KeyCode.Minus;
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (GameManager.instance.state != StatesManager.EGame.PAUSE) {
+			if ((Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl)) && Input.GetKeyDown(this.nextCamera)) {
+				// Next camera;
+			}
+			if ((Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl)) && Input.GetKeyDown(this.prevCamera)) {
+				// Prev Camera;
+			}
+		}
+	}
+}
