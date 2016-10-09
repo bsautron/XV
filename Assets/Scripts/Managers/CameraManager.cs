62277e96905e8e717dd9bd7b8903754ccaa57d00
Sebastien PARIAUD <spariaud@e3r12p19.42.fr>
2016-10-09 15:58:08 +0200

    managecamera: Minor

---
+++ Assets/Scripts/Managers/CameraManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CameraManager : Singleton<CameraManager> {
+
+	public List<Camera> cameras { 
+		get { return this.cameras; }
+		set { this.cameras = value; }
+	}
+
+	private Camera _currentCamera;
+
+	void Start() {
+		this._currentCamera = Camera.main;
+		Camera[] tmpCameras;
+		tmpCameras = GameObject.FindObjectsOfType<Camera> ();
+		for (int i = 0; i < tmpCameras.Length; i++) {
+			cameras.Add(tmpCameras[i]);
+		}
+	}
+
+	void NextCamera () {
+		this._currentCamera.depth = -1;
+	}
+}
