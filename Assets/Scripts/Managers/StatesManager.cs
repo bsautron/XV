7b4c06f1e4e15786de185309dde84e61605187a5
Basile Durst <bdurst2812@basiles-MacBook-Pro.local>
2016-11-16 15:18:28 +0100

    clickable: Delete assets

--- Assets/Scripts/Managers/StatesManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class StatesManager : Singleton<StatesManager> {
-
-	/* List all different State available Here */
-	public enum EBehavior {STANDBY, RUNNING};
-	public enum EObject {NOTSELECTED, SELECTED, NOTAVAILABLE};
-}
