4619f9bb7b1796a101cb233349ec90d930fc015e
bsautron <sautron.brunojj@gmail.com>
2016-12-06 01:51:39 +0100

    behavior: Walking set annimation

--- Assets/Scripts/Behaviors/Walking.cs
+++ Assets/Scripts/Behaviors/Walking.cs
@@ -6,13 +6,17 @@ public class Walking : ABehavior {
 
 	private Character 		_character;
 	private Vector3			_targetPosition;
+	private Animator		_animator;
 
 	public override void Start() {
 		base.Start ();
+
 		this._character = this._parent.GetComponent<Character> ();
+		this._animator = this._character.GetComponent<Animator>();
 	}
 
 	public override IEnumerator CoBehavior() {
+		this._animator.SetBool ("isWalking", true);
 		NavMeshAgent charaterAgent = this._character.agent;
 		Vector3 targetPosition = (Vector3)this._context;
 
@@ -25,9 +29,9 @@ public class Walking : ABehavior {
 			yield return false;
 		while (charaterAgent.remainingDistance > charaterAgent.stoppingDistance)
 			yield return true;
-
+		
+		this._animator.SetBool ("isWalking", false);
 		this.Stop ();
-
 		yield return new WaitForSeconds(2f);
 	}
 
